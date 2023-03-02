ca8161ea6e8be2078ec81db014e4c9948e91012b
Eric S. Londres <ericlondres@outlook.com>
2023-03-01 20:00:41 -0500

    install Tailwind and DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p class="text-3xl font-bold underline">This is another block of content.</p>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100)
+  }
+</style>
+
+
+<button class="btn">Hello from</button>
